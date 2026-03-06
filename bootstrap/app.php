bc4f7de256caa2927004af3d9b2a3009a67eb752
Antony Varas <antony.varastecsup.edu.pe>
2026-03-05 21:18:41 -0500

    Registro de middlewares en bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+            'CheckFirstLogin' => \App\Http\Middleware\CheckFirstLogin::class, // AGREGA ESTA LÍNEA
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
